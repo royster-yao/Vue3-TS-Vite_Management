b4a02d21cd8039662621ff6dfb79ae1731d911fe
royster <1172201059@qq.com>
2023-04-07 22:30:33 +0800

    Home component

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -7,24 +7,35 @@
         background-color="#545c64"
         class="el-menu-vertical-demo"
         default-active="2"
-        text-color="#fff">
-        <el-sub-menu index="1" v-for="menu in newMenus" :key="menu.id">
+        text-color="#fff"
+        :unique-opened="true"
+        :router="true">
+        <el-sub-menu
+          :index="menu.id + ''"
+          v-for="menu in newMenus"
+          :key="menu.id">
           <template #title>
             <span>{{ menu.title }}</span>
           </template>
           <template v-for="submenu in menu.children">
-            <el-menu-item index="1-4-1" v-if="submenu.hidden" :key="submenu.id">
+            <el-menu-item
+              :index="'/' + menu.name + '/' + submenu.name"
+              v-if="!submenu.hidden"
+              :key="submenu.id">
               {{ submenu.title }}
             </el-menu-item>
           </template>
         </el-sub-menu>
       </el-menu>
     </div>
-    <div class="home_content">右侧内容</div>
+    <div class="home_content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { computed } from "vue"
 import { useStore } from "vuex"
 
 interface MenuObj {
@@ -33,14 +44,16 @@ interface MenuObj {
   title: string
   hidden: 0 | 1
   children?: MenuObj[]
+  name: string
 }
 interface NewMenus {
   [key: number]: MenuObj
 }
 
 const store = useStore()
-const newMenus: NewMenus = store.getters.getNewMenus
-console.log(newMenus)
+const newMenus = computed<NewMenus>(() => {
+  return store.getters.getNewMenus
+})
 </script>
 
 <style lang="less" scoped>
