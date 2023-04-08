c291f7df658de3ed334d759fa4801d66fbf20792
royster <1172201059@qq.com>
2023-04-08 22:48:40 +0800

    axios api config

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,7 +5,7 @@ interface AdminLoginData {
     username: string
 }
 
-type PromiseRes<T> = Promise<MabageResult<T>>
+type PromiseRes<T = {}> = Promise<MabageResult<T>>
 
 interface MabageResult<T> {
     code: number,
@@ -21,11 +21,46 @@ interface AdminLoginRes {
 
 // 当前的用户信息
 interface AdminInfoRes {
-    menus: []
+    menus: [
+        {
+            id: number,
+            parentId: number
+        }
+    ]
+}
+
+interface AdminListParams {
+    keyword: string
+    pageNum: number
+    pageSize: number
 }
  
 // 登录返回token
 export const adminLoginApi = (data: AdminLoginData): PromiseRes<AdminLoginRes> => request.post("/admin/login",data)
 
-//获取当前登录的用户信息
-export const getAdminInfoApi = (): PromiseRes<AdminInfoRes> => request.get("/admin/info") 
+// 获取当前登录的用户信息
+export const getAdminInfoApi = (): PromiseRes<AdminInfoRes> => 
+request.get("/admin/info") 
+
+// 获取用户数据列表
+export const getAdminList = (data: AdminListParams): PromiseRes<{list: {}[]}> => 
+request.get('/admin/list',{params: data})
+
+// 修改用户xinxi
+export const updateAdmin = (id: number,data: AdminObjItf): PromiseRes => 
+request.post("/admin/update/" + id,data)
+
+// 获取所有角色
+export const getRoleListAll = ():PromiseRes<RoleObjItf[]> => 
+request.get("/role/listAll")
+
+// 根据用户id获取角色
+export const getAdminRoleById = (id: number): PromiseRes<RoleObjItf[]> => 
+request.get("/admin/role/" + id)
+
+// 分配用户角色
+export const updateAdminRole = (data: {adminId: number; roleIds: string}):PromiseRes => 
+request.post("/admin/role/update",null,{params: data})
+// form-data
+// export const updateAdminRole = (data: {adminId: number; roleIds: number[]}):PromiseRes => 
+// request.post("/admin/role/update",)
